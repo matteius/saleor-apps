ae368fa044777e5bd83817c9be0395822a2c31f8
Matt Davis <matteius@gmail.com>
2024-10-13 10:24:20 -0400

    Hack smtp app to use redis APL

--- apps/smtp/src/saleor-app.ts
+++ apps/smtp/src/saleor-app.ts
@@ -1,35 +1,12 @@
-import { APL, FileAPL, SaleorCloudAPL, UpstashAPL } from "@saleor/app-sdk/APL";
+import { RedisAPL } from "./apl";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 
-const aplType = process.env.APL ?? "file";
+export let apl: RedisAPL;
 
-export let apl: APL;
+apl = new RedisAPL({
+  url: process.env.REDIS_URL,
+});
 
-switch (aplType) {
-  case "upstash":
-    apl = new UpstashAPL();
-
-    break;
-  case "file":
-    apl = new FileAPL();
-
-    break;
-  case "saleor-cloud": {
-    if (!process.env.REST_APL_ENDPOINT || !process.env.REST_APL_TOKEN) {
-      throw new Error("Rest APL is not configured - missing env variables. Check saleor-app.ts");
-    }
-
-    apl = new SaleorCloudAPL({
-      resourceUrl: process.env.REST_APL_ENDPOINT,
-      token: process.env.REST_APL_TOKEN,
-    });
-
-    break;
-  }
-  default: {
-    throw new Error("Invalid APL config, ");
-  }
-}
 export const saleorApp = new SaleorApp({
   apl,
 });
