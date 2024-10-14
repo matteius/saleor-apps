8d995d51f442c7301d4bc85c325d32fa7a77bc8a
Matt Davis <matteius@gmail.com>
2024-10-14 06:53:12 -0400

    Deploy script

---
+++ scripts/deploy_latest_image.sh
@@ -0,0 +1,4 @@
+result=$(doctl registry repository list-tags saleor-smtp | grep latest | grep -o sha256:.* | xargs -i kubectl set image deployment/saleor-app-smtp-test saleor-app-smtp-test=registry.digitalocean.com/whitewhale/saleor-smtp@{} -n whitewhale)
+echo $result
+result=$(doctl registry repository list-tags saleor-stripe-app | grep latest | grep -o sha256:.* | xargs -i kubectl set image deployment/saleor-stripe-app saleor-stripe-app=registry.digitalocean.com/whitewhale/saleor-stripe-app{} -n whitewhale)
+echo $result
